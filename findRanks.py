22d1f040cc159a7b0bddc64a317ef6ce850d8274
Mark McDermott <markers920@gmail.com>
2011-05-25 17:03:41 -0400

    added score script

---
+++ findRanks.py
@@ -0,0 +1,84 @@
+
+import sqlite3
+import scipy
+from scipy import stats
+
+from ranks import *
+
+
+
+def main():
+    conn = sqlite3.connect('data/data.db')
+    conn.row_factory = sqlite3.Row
+
+    symb_sql = '''
+        SELECT `symbol`
+        FROM `symbols`
+    '''
+    symb_cur = conn.cursor()
+    symb_cur.execute(symb_sql)
+
+    comp_sql = '''
+        SELECT `date`, `open`,
+               `high`, `low`, `close`,
+               `volume`, `adj_close`
+        FROM `quotes`
+        WHERE `symbol` = ? AND
+              `date` > DATE('now','-30 days')
+        ORDER BY `date` DESC
+    '''
+    comp_cur = conn.cursor()
+
+    allScores = []
+    companyIndex = 0
+    for company in symb_cur:
+        comp_cur.execute(comp_sql, tuple(company))
+
+        timeVector  = []
+        openVector  = []
+        highVector  = []
+        lowVector   = []
+        closeVector = []
+        ii = 0
+
+        for row in comp_cur:
+            #make this based on row[1], (date entry)
+            timeVector.append(ii)
+
+            openVector.append(row['open'])
+            highVector.append(row['high'])
+            lowVector.append(row['low'])
+            closeVector.append(row['close'])
+            ii += 1
+
+        timeVector  = scipy.array(timeVector)
+        openVector  = scipy.array(openVector)
+        highVector  = scipy.array(highVector)
+        lowVector   = scipy.array(lowVector)
+        closeVector = scipy.array(closeVector)
+
+
+        s1 = expectedRatioGain(timeVector, openVector)
+        s2 = expectedRatioGain(timeVector, highVector)
+        s3 = expectedRatioGain(timeVector, lowVector)
+        s4 = expectedRatioGain(timeVector, closeVector)
+        avgS = (s1+s2+s3+s4) / 4.0
+
+
+        symbolScores = [companyIndex,company[0],s1,s2,s3,s4,avgS, max([s1,s2,s3,s4])]
+        print symbolScores
+        allScores.append( symbolScores )
+
+        companyIndex = companyIndex+1
+
+
+    for iii in range(2,len(symbolScores)):
+        print 'index ' + str(iii)
+        #allScores.sort(lambda x, y: cmp(x[iii],y[iii]))
+        allScores.sort(lambda x, y: cmp(y[iii],x[iii]))
+        print allScores
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ score_a_ranking_algorithm.py
@@ -0,0 +1,130 @@
+
+import sqlite3
+import scipy
+from scipy import stats
+
+from ranks import *
+
+
+
+def main():
+    conn = sqlite3.connect('data/data.db')
+    conn.row_factory = sqlite3.Row
+
+    symb_sql = '''
+        SELECT `symbol`
+        FROM `symbols`
+    '''
+    symb_cur = conn.cursor()
+    symb_cur.execute(symb_sql)
+
+
+
+    #units are samples, (days)
+    whole_history_size = 90	#pull this many days back
+    slide_size = 5		#when picking windows... shift this much
+    window_size = 30		#the history size presented to the ranking algorithm
+    max_own_window = 5		#maximum number of days that a purchase has to get to the predicted value (hold time)
+
+
+    comp_sql = '''
+        SELECT `date`, `open`,
+               `high`, `low`, `close`,
+               `volume`, `adj_close`
+        FROM `quotes`
+        WHERE `symbol` = ? AND
+              `date` > DATE('now','-90 days')
+        ORDER BY `date` DESC
+    '''
+    comp_cur = conn.cursor()
+
+
+    companyIndex = 0
+
+    #for each of the possible symbols
+    overallError = 0.0
+    allSymbolErrors = []
+    for company in symb_cur:
+        #print "PASSED: ", tuple([company[0], whole_history_size])
+        #comp_cur.execute( comp_sql, tuple([company[0], whole_history_size]) )	#grab entire history
+        comp_cur.execute(comp_sql, tuple(company))	#grab entire history
+
+
+        ####################################################################
+        ## step 1: build vectors of interest
+        ####################################################################
+        timeVector  = []
+        openVector  = []
+        highVector  = []
+        lowVector   = []
+        closeVector = []
+
+        #for each sample in this symbol's history
+        rowIndex = 0
+        for row in comp_cur:
+            timeVector.append(rowIndex)		#make this based on row[1], (date entry)
+
+            openVector.append(row['open'])
+            highVector.append(row['high'])
+            lowVector.append(row['low'])
+            closeVector.append(row['close'])
+            rowIndex += 1
+
+        timeVector  = scipy.array(timeVector)
+        openVector  = scipy.array(openVector)
+        highVector  = scipy.array(highVector)
+        lowVector   = scipy.array(lowVector)
+        closeVector = scipy.array(closeVector)
+
+        vectorOfInterest = closeVector
+        ####################################################################
+
+
+
+        ####################################################################
+        ## step 2: find this symbol's error term
+        ####################################################################
+        symbolError = 0.0
+        leftSide = 0
+        numberOfSlides = 0
+        while( leftSide < (whole_history_size - window_size - max_own_window) ):
+            rightSide = leftSide + window_size	#inclusive...
+            currentWindow = vectorOfInterest[leftSide:(rightSide+1)]
+
+            #insert the algorithm function to test
+            predicted_up_value = expectedRatioGain(timeVector, vectorOfInterest)
+
+            #find the value closest to this prediction, within the next few days
+            #and call their difference the error term
+            futureValues = vectorOfInterest[(rightSide+1):(rightSide+1+max_own_window)]
+            currentValue = currentWindow[len(currentWindow)-1]						#last element
+
+            #find the minimum absolute value difference between 
+            #the actual and predicted over the next few days
+            error_term = 0.0
+            #for each of the next few days
+            for futureIndex in range(0,len(futureValues)):
+                actual_percentage_up = (futureValues[futureIndex] - currentValue) / currentValue
+                possible_replacement = abs( predicted_up_value - actual_percentage_up )
+                if( (futureIndex == 0) or (possible_replacement < error_term) ):
+                    error_term = possible_replacement
+
+            symbolError = symbolError + error_term
+
+            leftSide = leftSide + window_size
+            numberOfSlides = numberOfSlides + 1
+
+        symbolError = symbolError / numberOfSlides							#expected value
+        ####################################################################
+
+        print company, ": ", symbolError
+        allSymbolErrors.append( symbolError )
+        overallError = overallError + symbolError
+
+    print "allSymbolErrors: ", allSymbolErrors
+    print "overallError: ", overallError
+
+
+if __name__ == '__main__':
+    main()
+
